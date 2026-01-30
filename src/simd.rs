cc64f5492cef9d1cf4fd6963ee65d4573b23162f
George Burton <burtonageo@gmail.com>
2026-01-30 16:33:58 +0000

    Add more simd features

--- src/simd.rs
+++ src/simd.rs
@@ -1,10 +1,14 @@
-
 // SPDX-License-Identifier: MIT OR Apache-2.0
 
+use crate::{
+    utils::num::{ClosedAdd, ClosedMul, ClosedNeg, ClosedSub, Sqrt, Zero},
+    vector::Vector,
+};
 use core::{
     borrow::{Borrow, BorrowMut},
     convert::{AsMut, AsRef},
     ops::{Add, Deref, DerefMut, Div, Mul, Sub},
+    simd::{Simd, SimdElement},
 };
 
 pub trait SimdMul<U = Self>: Copy {
@@ -31,6 +35,7 @@ pub trait SimdSub<U = Self>: Copy {
     fn simd_sub(self, rhs: U) -> Self::Output;
 }
 
+#[repr(transparent)]
 #[derive(Clone, Copy, Debug, Eq, Hash, PartialEq)]
 pub struct SimdValue<T>(pub T);
 
@@ -113,5 +118,76 @@ impl<T: SimdSub<U>, U> Sub<SimdValue<U>> for SimdValue<T> {
     #[inline]
     fn sub(self, rhs: SimdValue<U>) -> Self::Output {
         SimdValue(self.0.simd_sub(rhs.0))
+    }
+}
+
+impl<T: SimdMul<U>, U> SimdMul<SimdValue<U>> for SimdValue<T> {
+    type Output = SimdValue<T::Output>;
+    #[inline]
+    fn simd_mul(self, rhs: SimdValue<U>) -> Self::Output {
+        SimdValue(self.0.simd_mul(rhs.0))
+    }
+}
+
+impl<T: SimdDiv<U>, U> SimdDiv<SimdValue<U>> for SimdValue<T> {
+    type Output = SimdValue<T::Output>;
+    #[inline]
+    fn simd_div(self, rhs: SimdValue<U>) -> Self::Output {
+        SimdValue(self.0.simd_div(rhs.0))
+    }
+}
+
+impl<T: SimdAdd<U>, U> SimdAdd<SimdValue<U>> for SimdValue<T> {
+    type Output = SimdValue<T::Output>;
+    #[inline]
+    fn simd_add(self, rhs: SimdValue<U>) -> Self::Output {
+        SimdValue(self.0.simd_add(rhs.0))
+    }
+}
+
+impl<T: SimdSub<U>, U> SimdSub<SimdValue<U>> for SimdValue<T> {
+    type Output = SimdValue<T::Output>;
+    #[inline]
+    fn simd_sub(self, rhs: SimdValue<U>) -> Self::Output {
+        SimdValue(self.0.simd_sub(rhs.0))
+    }
+}
+
+impl<T: SimdElement + ClosedAdd + Zero, const N: usize> SimdValue<Vector<T, N>>
+where
+    Simd<T, N>: ClosedMul,
+{
+    #[must_use]
+    #[inline]
+    pub fn dot<I: Into<SimdValue<Vector<T, N>>>>(self, rhs: I) -> T {
+        self.0.simd_dot(rhs)
+    }
+
+    #[must_use]
+    #[inline]
+    pub fn len_squared(self) -> T {
+        self.0.simd_len_squared()
+    }
+}
+
+impl<T: SimdElement + ClosedAdd + Zero + Sqrt, const N: usize> SimdValue<Vector<T, N>>
+where
+    Simd<T, N>: ClosedMul,
+{
+    #[must_use]
+    #[inline]
+    pub fn len(self) -> T {
+        self.0.simd_len()
+    }
+}
+
+impl<T: SimdElement + ClosedNeg> SimdValue<Vector<T>>
+where
+    Simd<T, 3>: ClosedMul + ClosedSub,
+{
+    #[must_use]
+    #[inline]
+    pub fn cross<I: Into<SimdValue<Vector<T>>>>(self, rhs: I) -> Self {
+        SimdValue(self.0.simd_cross(rhs))
     }
 }
